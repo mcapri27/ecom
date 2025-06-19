def4933e5881280993fef4fd7372e76cf0bcf10e
__ndala__ <juanlyonne06@gmail.com>
2025-06-19 21:19:16 +0200

    Add files via upload

---
+++ store/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import *
+
+# Create your views here.
+def home(request):
+    products = Product.objects.all()
+    context = {'products': products}
+    return render(request, "index.html", context)
